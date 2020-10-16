3fcc40e0c3f604de674bbe5573eaef1bfb6447a8
wisnubaldas <wisnubaldas@gmail.com>
2020-10-16 14:10:03 +0700

    umkm

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['namespace' => 'Frontend', 'as' => 'frontend.'], function () {
+    include_route_files(__DIR__.'/frontend/');
+});
+
+// Auth::routes();
+
+Route::group(['namespace' => 'Backend', 'prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth'], function () {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+    include_route_files(__DIR__.'/backend/');
+    
+});
